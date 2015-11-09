194180b3872c0c0fe9d4f0d429641c000ed1534e
Jeffrey Killeen <jtk12b@acu.edu>
2015-11-09 13:33:12 -0600

    Added to seed file and finished recommendation engine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,18 @@ class UsersController < ApplicationController
 			possibilities = @user.items.map{|i| i.categories}
 			userCategories = possibilities.reduce{|a,b| a.concat b}
 			@userCatFilter = userCategories.uniq{|item| item.name}.map{|n| n.name}
+			@recommended = @recommended.map{|a| ItemCat.new(a.name, a.categories.map{|b| b.name})}
+			@recommended = @recommended.select{|item| (item.categoryList & @userCatFilter).length > 0}
+			@recommended.sort!{|a,b| (b.categoryList & @userCatFilter).length <=> (a.categoryList & @userCatFilter).length}
+			#@recommended = @recommended.reduce{|a,b| a.concat b}.uniq{|item| item}
 			#@recommended = @recommended.select{|item| (item.categories & @userCatFilter).length > 0}
-			@recommended = @recommended.map{|a| a.categories.map{|b| b.name}}
-			#calculate all categories
+			
 			purchased = @user.items
 			@purchasedList = []
 			purchased.each do |p|
 				categories = p.categories.map{|a| a.name}
-				catList = categories.inject{|cat1, cat2| cat1 + ", " + cat2}
-				@purchasedList << ItemCat.new(p.name, catList)
+				#catList = categories.inject{|cat1, cat2| cat1 + ", " + cat2}
+				@purchasedList << ItemCat.new(p.name, categories)
 			end
 		end
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+jersey = Item.find_by(name: "Adidas Jersey")
+Item.create!(name: "Nike Dunks")
+dunks = Item.find_by(name: "Nike Dunks")
+Item.create!(name: "Nike Street Basketball")
+nikebasketball = Item.find_by(name: "Nike Street Basketball")
+basketball = Category.find_by(name: "Basketball")
+Category.create!(name: "Adidas")
+adidas = Category.find_by(name: "Adidas")
+john = User.find_by(name: "John Smith")
+User.create!(name: "David Jones")
+david = User.find_by(name: "David Jones")
+Category.create!(name: "Nike")
+nike = Category.find_by(name: "Nike")
+Category.create!(name: "Clothing")
+clothing = Category.find_by(name: "Clothing")
+Category.create!(name: "Shoes")
+shoes = Category.find_by(name: "Shoes")
+Category.create!(name: "Balls")
+balls = Category.find_by(name: "Balls")
+
+jersey.categories << adidas
+jersey.save
+adidas.items << jersey
+adidas.save
+
+jersey.categories << basketball
+jersey.save
+basketball.items << jersey
+basketball.save
+
+jersey.categories << clothing
+jersey.save
+clothing.items << jersey
+clothing.save
+
+dunks.categories << shoes
+dunks.save
+shoes.items << dunks
+shoes.save
+
+dunks.categories << nike
+dunks.save
+nike.items << dunks
+nike.save
+
+dunks.categories << basketball
+dunks.save
+basketball.items << dunks
+basketball.save
+
+nikebasketball.categories << basketball
+nikebasketball.save
+basketball.items << nikebasketball
+basketball.save
+
+nikebasketball.categories << nike
+nikebasketball.save
+nike.items << nikebasketball
+nike.save
+
+nikebasketball.categories << balls
+nikebasketball.save
+balls.items << nikebasketball
+balls.save
+
+john.items << jersey
+john.save
+jersey.users << john
+jersey.save
+
+david.items << dunks
+david.save
+dunks.users << david
+dunks.save
